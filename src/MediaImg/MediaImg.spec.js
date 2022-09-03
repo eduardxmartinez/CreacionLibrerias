5f8eb334ff2a69c451fdf8c9c991b68760939ec3
eduardxmartinez <eduardom17809@gmail.com>
2022-09-03 14:48:39 -0700

    MediaImg test

--- src/MediaImg/MediaImg.spec.js
+++ src/MediaImg/MediaImg.spec.js
@@ -1,2 +1,15 @@
 import { render } from '@testing-library/react'
 import MediaImg from './MediaImg'
+
+describe("@components/MediaImg", () => {
+    it("asfasfas", () => {
+        // arrange
+        const {getByRole} = render(
+            <MediaImg alt="Platzi" src="http://www.platzi.com/img"/>
+        )
+        // act
+        const MediaImgTest = getByRole("img")
+        // assert
+        expect(MediaImgTest).toBeInTheDocument();
+    })
+})
